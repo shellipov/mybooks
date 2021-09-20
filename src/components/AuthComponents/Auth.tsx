8d9c26616e9c88bad735630497c88a50d3b41502
shell <shellipov@mail.ru>
2021-09-20 04:29:01 +0300

    something

---
+++ src/components/AuthComponents/Auth.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useState } from "react";
+import Login from "./LogIn";
+import SignIn from "./SignIn";
+import Button from "../UI/Button/Button";
+import styles from "./style.module.scss";
+
+const Auth: FC = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.auth_back_block}>
+        <span className="margin"> У меня есть аккаунт</span>
+        <Button color="red" onClick={() => setIsLogin(!isLogin)}>
+          {"Войти"}
+        </Button>
+      </div>
+      <div className={styles.auth_back_block}>
+        <span className="margin"> Я новый пользователь</span>
+
+        <Button onClick={() => setIsLogin(!isLogin)}>
+          {"Зарегистрироваться"}
+        </Button>
+      </div>
+      <div className={[styles.auth_front_block, isLogin ? styles.active  : ""].join(" ")}>
+        {isLogin ? <SignIn /> : <Login />}
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
